1bc6e58a2834832a40f8e5037d4b088304c3d8ba
aaron <agray2195@gmail.com>
2024-02-26 15:01:03 -0600

    Test Initialization

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using SIREN.Models;
+
+namespace Aaron_Gray.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IWebHostEnvironment _hostingEnvironment;
+
+        public HomeController(ILogger<HomeController> logger, IWebHostEnvironment hostingEnvironment)
+        {
+            _logger = logger;
+            _hostingEnvironment = hostingEnvironment;
+        }
+
+        public ActionResult DisplayImage(string imageName)
+        {
+            string imagePath = Path.Combine(_hostingEnvironment.WebRootPath, "cards", imageName);
+            return PhysicalFile(imagePath, "image/png"); // Adjust the content type based on the image type
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}

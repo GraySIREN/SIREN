8f8a9d73a5426b03ca8e531177430b2d91fa2ccb
aaron <agray2195@gmail.com>
2024-03-13 21:46:06 -0500

    Fixed some styling, refined some of my js functions. Condensed and refined some the class names and ids.

--- wwwroot/js/taskmanager.js
+++ wwwroot/js/taskmanager.js
@@ -6,51 +6,50 @@ function setTheme(theme) {
         document.documentElement.style.setProperty('--theme-task-container-background-color', '#F2F2F2');
         document.documentElement.style.setProperty('--theme-text-color', '#262626');
         document.documentElement.style.setProperty('--theme-border-color', '#737373');
-        document.documentElement.style.setProperty('--theme-grid-item-hover-background-color', '#BFBFBF');
-        document.documentElement.style.setProperty('--theme-grid-item-hover-text-color', '#262626');
+        document.documentElement.style.setProperty('--theme-draggable-hover-background-color', '#BFBFBF');
+        document.documentElement.style.setProperty('--theme-draggable-hover-text-color', '#262626');
     }
     else if (theme === 'theme2') { //ROSE GOLD
         document.documentElement.style.setProperty('--theme-page-background-color', '#F2D5CE');
         document.documentElement.style.setProperty('--theme-task-container-background-color', '#F2BFBB');
         document.documentElement.style.setProperty('--theme-text-color', '#D98B84');
         document.documentElement.style.setProperty('--theme-border-color', '#F2F2F2');
-        document.documentElement.style.setProperty('--theme-grid-item-hover-background-color', '#F2BDC7');
-        document.documentElement.style.setProperty('--theme-grid-item-hover-text-color', '#BF495E');
+        document.documentElement.style.setProperty('--theme-draggable-hover-background-color', '#F2BDC7');
+        document.documentElement.style.setProperty('--theme-draggable-hover-text-color', '#BF495E');
     } else if (theme === 'theme3') { //LIME GREEN
         document.documentElement.style.setProperty('--theme-page-background-color', '#B5D991');
         document.documentElement.style.setProperty('--theme-task-container-background-color', '#B5D991');
         document.documentElement.style.setProperty('--theme-text-color', '#F0F1F2');
         document.documentElement.style.setProperty('--theme-border-color', '#71A64B');
-        document.documentElement.style.setProperty('--theme-grid-item-hover-background-color', '#DDF2CE');
-        document.documentElement.style.setProperty('--theme-grid-item-hover-text-color', '#71A64B');
+        document.documentElement.style.setProperty('--theme-draggable-hover-background-color', '#DDF2CE');
+        document.documentElement.style.setProperty('--theme-draggable-hover-text-color', '#71A64B');
     } else if (theme === 'theme4') { //DARK MODE
         document.documentElement.style.setProperty('--theme-page-background-color', '#1F1F1F');
         document.documentElement.style.setProperty('--theme-task-container-background-color', '#000000');
         document.documentElement.style.setProperty('--theme-text-color', '#BB86FC');
         document.documentElement.style.setProperty('--theme-border-color', '#03DAC6');
-        document.documentElement.style.setProperty('--theme-grid-item-hover-background-color', '#BB86FC');
-        document.documentElement.style.setProperty('--theme-grid-item-hover-text-color', '#000000');
+        document.documentElement.style.setProperty('--theme-draggable-hover-background-color', '#BB86FC');
+        document.documentElement.style.setProperty('--theme-draggable-hover-text-color', '#000000');
     } else if (theme === 'theme5') { //LIGHT MODE
         document.documentElement.style.setProperty('--theme-page-background-color', '#E9F0F2');
         document.documentElement.style.setProperty('--theme-task-container-background-color', '#CCDBE3');
         document.documentElement.style.setProperty('--theme-text-color', '#6092A6');
         document.documentElement.style.setProperty('--theme-border-color', '#E7E3FD');
-        document.documentElement.style.setProperty('--theme-grid-item-hover-background-color', '#8BCAD9');
-        document.documentElement.style.setProperty('--theme-grid-item-hover-text-color', '#E9F0F2');
+        document.documentElement.style.setProperty('--theme-draggable-hover-background-color', '#8BCAD9');
+        document.documentElement.style.setProperty('--theme-draggable-hover-text-color', '#E9F0F2');
     } else if (theme === 'theme6') { //RETRO
         document.documentElement.style.setProperty('--theme-page-background-color', '#037E8C');
         document.documentElement.style.setProperty('--theme-task-container-background-color', '#024959');
         document.documentElement.style.setProperty('--theme-text-color', '#FFFFFA');
         document.documentElement.style.setProperty('--theme-border-color', '#F2F2BF');
-        document.documentElement.style.setProperty('--theme-grid-item-hover-background-color', '#024959');
-        document.documentElement.style.setProperty('--theme-grid-item-hover-text-color', '#F24C27');
+        document.documentElement.style.setProperty('--theme-draggable-hover-background-color', '#024959');
+        document.documentElement.style.setProperty('--theme-draggable-hover-text-color', '#F24C27');
     }
 }
 
 function addTaskRow() {
     // Create a new task grid item
     const newTask = document.createElement('div');
-    newTask.setAttribute('id', 'grid-item');
     newTask.setAttribute('contenteditable', 'true');
     newTask.setAttribute('draggable', 'true');
     newTask.textContent = 'New Task';
@@ -61,9 +60,10 @@ function addTaskRow() {
     newTask.addEventListener('dragend', () => {
         newTask.classList.remove('dragging')
     })
+    
 
-    // Append the new task grid item to the grid container
-    document.getElementById('task-container').appendChild(newTask);
+    const taskContainer = document.querySelector('.task-container');
+    taskContainer.appendChild(newTask);
 }
 
 function deleteTaskRow() {
@@ -71,11 +71,10 @@ function deleteTaskRow() {
     const gridContainer = document.getElementById('task-container');
 
     // Remove the last task grid item if it exists
-    if (gridContainer.lastElementChild.id === 'grid-item') {
+    if (gridContainer.lastElementChild.classList === 'draggable') {
         gridContainer.removeChild(gridContainer.lastElementChild);
     }
 }
-
 
 //Creating draggable element to HTML
 

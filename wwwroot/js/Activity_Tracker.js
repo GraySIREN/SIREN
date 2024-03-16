ef06e8bf55d3632689272fb2dcf4ce9597ba3743
aaron <agray2195@gmail.com>
2024-03-16 18:09:19 -0500

    Added various icons, further styling and formatting.

--- wwwroot/js/Activity_Tracker.js
+++ wwwroot/js/Activity_Tracker.js
@@ -14,39 +14,3 @@ setInterval(updateTime, 1000);
 
 // Initial call to display time immediately
 updateTime();
-
-
-
-
-
-
-// Trackm-item5 scrollable Icon Bank//
-const iconBank = document.getElementById('trackm5'); // Corrected ID here
-let isMouseDown = false;
-let startX;
-let startY;
-let scrollLeft;
-let scrollTop;
-
-iconBank.addEventListener('mousedown', (e) => {
-    isMouseDown = true;
-    startX = e.pageX - iconBank.offsetLeft;
-    startY = e.pageY - iconBank.offsetTop;
-    scrollLeft = iconBank.scrollLeft;
-    scrollTop = iconBank.scrollTop;
-});
-
-document.addEventListener('mouseup', () => {
-    isMouseDown = false;
-});
-
-document.addEventListener('mousemove', (e) => {
-    if (!isMouseDown) return;
-    e.preventDefault();
-    const x = e.pageX - iconBank.offsetLeft;
-    const y = e.pageY - iconBank.offsetTop;
-    const walkX = (x - startX) * 3; // Adjust the scrolling speed if needed
-    const walkY = (y - startY) * 3;
-    iconBank.scrollLeft = scrollLeft - walkX;
-    iconBank.scrollTop = scrollTop - walkY;
-});

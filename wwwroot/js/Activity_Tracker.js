b844db1e43fdc0aaf64eb9b1674dca27f059fc06
aaron <agray2195@gmail.com>
2024-03-20 16:08:08 -0500

    Adjusted some site wide styling, changed footer position to relative, allowed wider space for containers within body.

--- wwwroot/js/Activity_Tracker.js
+++ wwwroot/js/Activity_Tracker.js
@@ -1,16 +1,16 @@
 ﻿function updateTime() {
     const now = new Date();
-    const hours = String(now.getHours()).padStart(2, '0');
+    let hours = now.getHours();
     const minutes = String(now.getMinutes()).padStart(2, '0');
     const seconds = String(now.getSeconds()).padStart(2, '0');
     const date = now.toDateString();
 
+    // Convert hours to 12-hour format and determine AM/PM
+    const amPm = hours >= 12 ? 'PM' : 'AM';
+    hours = hours % 12 || 12;
+
     const clockElement = document.getElementById('live-datetime');
-    clockElement.textContent = `${date} ${hours}:${minutes}:${seconds}`;
+    clockElement.textContent = `${date} ${hours}:${minutes}:${seconds} ${amPm}`;
 }
-
-// Update time every second
 setInterval(updateTime, 1000);
-
-// Initial call to display time immediately
 updateTime();
